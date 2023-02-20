a0dd8704620b1ca806928a3221eb02fd147611fc
Emem Ekpo <ememekpo@yahoo.com>
2023-02-20 20:41:19 +0100

    setup validation for user model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -5,7 +5,7 @@ class Expense < ApplicationRecord
   has_many: group_expenses, dependent: :destroy
   has_many: :groups, through: :group_budgets, dependent: :destroy
 
-  validates: :name, present: true, length: { minimum: 1, maximum 20 }, uniqueness: { scope: :author, message: 'You already have a transaction with this name' }
-  validates: :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, present: true, length: { minimum: 1, maximum 20 }, uniqueness: { scope: :author, message: 'You already have a transaction with this name' }
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :groups, foreign_key: :author_id, dependent: :destroy
   has_many :expenses, foreign_key: :author_id, dependent: :destroy
 
+  validates :name, prsence: true, length: {minumum: 1, maximum: 20 }
+  
 end
