b80ffb9ba8ca71bc97fb0db047a198b5f140533f
Emem Ekpo <ememekpo@yahoo.com>
2023-02-20 18:27:46 +0100

    Add association to expense.rb file

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,8 @@
 class Expense < ApplicationRecord
   belongs_to :author
+
+  belongs_to: author, class_name: 'User'
+  has_many: grou_expenses, dependent: :destroy
+  has_many :groups, through: :group_budgets, dependent: :destroy
+
 end
