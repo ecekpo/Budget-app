26abcad09033cfcd53ddcfa2d3e1b1a913638c4d
Emem Ekpo <ememekpo@yahoo.com>
2023-02-20 18:49:43 +0100

    Add route for user group and expense

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :group_expenses
-  resources :expenses
-  resources :groups
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
+  
+  resources :users do
+  resources :categories, controller: 'groups', as: 'groups' do
+  resources :transactions, controller: 'expenses', as: 'expenses'
+    end
+  end
 end
--- db/migrate/20230220172116_create_groups.rb
+++ db/migrate/20230220172116_create_groups.rb
@@ -3,7 +3,7 @@ class CreateGroups < ActiveRecord::Migration[7.0]
     create_table :groups do |t|
       t.string :name
       t.string :icon, default: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F285655%2Fuser_icon&psig=AOvVaw37joxjOi9Q-l0wVxr1h-Fm&ust=1677000866763000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKDu9tTRpP0CFQAAAAAdAAAAABAE'
-      t.references :author, null: false, foreign_key: {to_table; users}
+      t.references :author, null: false, foreign_key: {to_table: users}
 
       t.timestamps
     end
