bfe3a5c957e3670bcdacbeeea9fd384b1f52ea39
Emem Ekpo <ememekpo@yahoo.com>
2023-02-21 04:44:22 +0100

    Add group rspec unit test

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,11 +1,8 @@
 class Expense < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
+  has_many :group_expenses, dependent: :destroy
+  has_many :groups, through: :group_budgets, dependent: :destroy
 
-  belongs_to: author, class_name: 'User'
-  has_many: group_expenses, dependent: :destroy
-  has_many: :groups, through: :group_budgets, dependent: :destroy
-
-  validates :name, present: true, length: { minimum: 1, maximum 20 }, uniqueness: { scope: :author, message: 'You already have a transaction with this name' }
+  validates :name, presence: true, length: { minimum: 1, maximum: 20 }, uniqueness: { scope: :author, message: 'You already have a transaction with this name' }
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
-
 end
